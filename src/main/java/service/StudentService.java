c6342a93495f4825515a1d54ba21bdfe352ff215
divakaran5421 <divakaran54210@gmail.com>
2023-08-05 13:01:11 +0530

    modified...

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -17,14 +17,27 @@ public class StudentService {
         Student student = new Student();
         List<Student> studentList = new ArrayList<Student>();
         student.setStudentId("001");
-        student.setName("name");
-        student.setCourse("MCA");
+        student.setName("Dhamodharan");
+        student.setCourse("BCA");
         student.setStartDate("01/08/2023");
         student.setEndDate("08/01/2026");
         studentList.add(student);
 
+        Student student1 = new Student();
+        student1.setStudentId("002");
+        student1.setName("Divakaran");
+        student1.setCourse("BCA");
+        student1.setStartDate("01/08/2023");
+        student1.setEndDate("08/01/2026");
+        studentList.add(student1);
 
-
+        Student student2 = new Student();
+        student2.setStudentId("003");
+        student2.setName("Srihari");
+        student2.setCourse("BCA");
+        student2.setStartDate("01/08/2023");
+        student2.setEndDate("08/01/2026");
+        studentList.add(student2);
     }
 
     public List<Student> getStudents(){
